ebb9b128d0eea8a16473c2be8c59cd6fc11fb192
abhijarvis <abhishekpandit740@gmail.com>
2025-02-23 15:48:28 +0530

    Extract file

--- check.py
+++ check.py
@@ -1,2 +1,24 @@
-print('Abhishek')
-print('kumar')
+print('DataScrping')
+import urllib.request
+
+# URL of the web page to fetch
+url=input('Input the URL to scrap - > ')
+
+try:
+    # Open the URL and read its content
+    response = urllib.request.urlopen(url)
+    
+    # Read the content of the response
+    data = response.read()
+    
+    # Decode the data (if it's in bytes) to a string
+    html_content = data.decode('utf-8')
+    
+    # Print the HTML content of the web page
+    print(html_content)
+    file1 = open("extract.txt", "w")
+    file1.writelines(html_content)
+    file1.close()
+
+except Exception as e:
+    print("Error fetching URL:", e)
